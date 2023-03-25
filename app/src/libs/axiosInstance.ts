49ac7f11ff8b6e92f39c3784262ba89357fd49b9
yyyoichi <origamiichiro@gmail.com>
2023-03-25 13:09:11 +0900

    add axios

---
+++ app/src/libs/axiosInstance.ts
@@ -0,0 +1,20 @@
+import axios, { AxiosInstance, AxiosRequestConfig } from 'axios';
+
+export class TokenizedFetch {
+  readonly instance: AxiosInstance;
+  constructor() {
+    this.instance = axios.create({
+      baseURL: 'http://127.0.0.1:8080',
+      withCredentials: true,
+    });
+  }
+  async safe(options: AxiosRequestConfig) {
+    const res = await this.instance.get('/safe');
+    const token = res.headers['x-csrf-token'];
+    options.headers = {
+      ...options.headers,
+      'X-Csrf-Token': token,
+    };
+    return await this.instance(options);
+  }
+}
