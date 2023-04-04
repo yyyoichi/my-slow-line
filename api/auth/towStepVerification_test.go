75d9ea5fb2f6ca4301ae8661621e7de87cb6d2cc
yyyoichi <origamiichiro@gmail.com>
2023-04-04 11:05:34 +0000

    add two-step verification code gen and test

---
+++ api/auth/towStepVerification_test.go
@@ -0,0 +1,54 @@
+package auth
+
+import (
+	"testing"
+	"time"
+)
+
+func TestTwoVerification(t *testing.T) {
+	input := GenerateRandomSixNumber()
+	test := []struct {
+		code string
+		add  time.Duration
+		exp  bool
+	}{
+
+		{ // normal
+			code: input,
+			add:  -time.Minute * 9,
+			exp:  true,
+		},
+		{ // normal
+			code: input,
+			add:  -time.Second * 599,
+			exp:  true,
+		},
+		{ // abnormal
+			code: input,
+			add:  -time.Second * 600,
+			exp:  false,
+		},
+		{ // abnormal
+			code: GenerateRandomSixNumber(),
+			add:  -time.Minute * 9,
+			exp:  false,
+		},
+		{ // abnormal
+			code: GenerateRandomSixNumber(),
+			add:  -time.Second * 601,
+			exp:  false,
+		},
+		{ // abnormal
+			code: GenerateRandomSixNumber(),
+			add:  time.Second * 1,
+			exp:  false,
+		},
+	}
+	for i, tt := range test {
+		loginAt := time.Now().Add(tt.add)
+		result := VerificateSixNumber(input, tt.code, loginAt)
+		if result != tt.exp {
+			t.Errorf("%d: expected '%v' but got='%v' code is '%s' loginAt is before '%v'", i, tt.exp, result, tt.code, tt.add)
+		}
+	}
+}
