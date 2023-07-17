e15646ec16e6a9a095ed06a347b2fc8b5d0aa1b0
yyyoichi <origamiichiro@gmail.com>
2023-07-17 04:11:28 +0000

    feat add chat querys

---
+++ api/services/database/chat.go
@@ -0,0 +1,209 @@
+package database
+
+import (
+	"time"
+)
+
+type ChatSession struct {
+	ID        int
+	UserID    int
+	PublicKey string
+	Name      string
+	CreateAt  time.Time
+	UpdateAt  time.Time
+	Deleted   bool
+}
+
+type ChatSessionRepository struct{}
+
+func (csr *ChatSessionRepository) QueryByUserID(userID int) ([]ChatSession, error) {
+	// query
+	query := `SELECT id, user_id, public_key, name, create_at, update_at, deleted FROM chat_sessions WHERE user_id = ?`
+	rows, err := DB.Query(query, userID)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	// results
+	var results []ChatSession
+	for rows.Next() {
+		cs := ChatSession{}
+		err := rows.Scan(&cs.ID, &cs.UserID, &cs.PublicKey, &cs.Name, &cs.CreateAt, &cs.UpdateAt, &cs.Deleted)
+		if err != nil {
+			return nil, err
+		}
+		results = append(results, cs)
+	}
+
+	if err := rows.Err(); err != nil {
+		return nil, err
+	}
+
+	return results, nil
+}
+
+func (csr *ChatSessionRepository) UpdateName(id int, name string) error {
+	// query
+	query := `UPDATE chat_sessions SET name = ?, update_at = NOW() WHERE id = ?`
+	_, err := DB.Exec(query, name, id)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (csr *ChatSessionRepository) Create(userID int, publicKey string, name string) error {
+	// query
+	query := `INSERT INTO chat_sessions (user_id, public_key, name) VALUES (?, ?, ?)`
+	_, err := DB.Exec(query, userID, publicKey, name)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (csr *ChatSessionRepository) DeleteAll(userID int) error {
+	// query
+	query := `DELETE FROM chat_sessions WHERE user_id = ?`
+	_, err := DB.Exec(query, userID)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+/////////////////////////////
+//chat session participants//
+/////////////////////////////
+
+type ChatSessionParticipant struct {
+	ID        int
+	SessionID int
+	UserID    int
+	CreateAt  time.Time
+	UpdateAt  time.Time
+	Deleted   bool
+}
+
+type ChatSessionParticipantRepository struct{}
+
+func (cspr *ChatSessionParticipantRepository) QueryBySessionID(sessionID int) ([]ChatSessionParticipant, error) {
+	// query
+	query := `SELECT id, chat_session_id, user_id, create_at, update_at, deleted FROM chat_session_participants WHERE chat_session_id = ?`
+	rows, err := DB.Query(query, sessionID)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	// results
+	var results []ChatSessionParticipant
+	for rows.Next() {
+		csp := ChatSessionParticipant{}
+		err := rows.Scan(&csp.ID, &csp.SessionID, &csp.UserID, &csp.CreateAt, &csp.UpdateAt, &csp.Deleted)
+		if err != nil {
+			return nil, err
+		}
+		results = append(results, csp)
+	}
+
+	if err := rows.Err(); err != nil {
+		return nil, err
+	}
+
+	return results, nil
+}
+
+func (cspr *ChatSessionParticipantRepository) Create(sessionID int, userID int) error {
+	// query
+	query := `INSERT INTO chat_session_participants (chat_session_id, user_id) VALUES (?, ?)`
+	_, err := DB.Exec(query, sessionID, userID)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (cspr *ChatSessionParticipantRepository) DeleteAll(sessionID int) error {
+	// query
+	query := `DELETE FROM chat_session_participants WHERE chat_session_id = ?`
+	_, err := DB.Exec(query, sessionID)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+/////////////////////////////
+//         chats           //
+/////////////////////////////
+
+type Chat struct {
+	ID        int
+	SessionID int
+	UserID    int
+	Content   string
+	CreateAt  time.Time
+	UpdateAt  time.Time
+	Deleted   bool
+}
+
+type ChatRepository struct{}
+
+func (cr *ChatRepository) QueryBySessionID(sessionID int) ([]Chat, error) {
+	// query
+	query := `SELECT id, chat_session_id, user_id, content, create_at, update_at, deleted FROM chats WHERE chat_session_id = ?`
+	rows, err := DB.Query(query, sessionID)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	// results
+	var results []Chat
+	for rows.Next() {
+		c := Chat{}
+		err := rows.Scan(&c.ID, &c.SessionID, &c.UserID, &c.Content, &c.CreateAt, &c.UpdateAt, &c.Deleted)
+		if err != nil {
+			return nil, err
+		}
+		results = append(results, c)
+	}
+
+	if err := rows.Err(); err != nil {
+		return nil, err
+	}
+
+	return results, nil
+}
+
+func (cr *ChatRepository) Create(sessionID int, userID int, content string) error {
+	// query
+	query := `INSERT INTO chats (chat_session_id, user_id, content) VALUES (?, ?, ?)`
+	_, err := DB.Exec(query, sessionID, userID, content)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (cr *ChatRepository) DeleteAll(sessionID int) error {
+	// query
+	query := `DELETE FROM chats WHERE chat_session_id = ?`
+	_, err := DB.Exec(query, sessionID)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (cr *ChatRepository) DeleteBySessionAndUser(sessionID int, userID int) error {
+	// query
+	query := `DELETE FROM chats WHERE chat_session_id = ? AND user_id = ?`
+	_, err := DB.Exec(query, sessionID, userID)
+	if err != nil {
+		return err
+	}
+	return nil
+}
---
+++ api/services/database/chat_test.go
@@ -0,0 +1,202 @@
+package database
+
+import (
+	"testing"
+)
+
+func TestChatSession(t *testing.T) {
+
+	// Create a mock user
+	usersR := &UserRepository{}
+	mockUser := createMockUser()
+	testUser, close := userMock(t, usersR, mockUser)
+	defer close()
+
+	// Create the ChatSessionRepository instance
+	csr := &ChatSessionRepository{}
+
+	// Test Create method
+	err := csr.Create(testUser.Id, "public_key", "Test Chat Session")
+	if err != nil {
+		t.Errorf("Error creating chat session: %s", err.Error())
+	}
+
+	// Test QueryByUserID method
+	sessions, err := csr.QueryByUserID(testUser.Id)
+	if err != nil {
+		t.Errorf("Error querying chat sessions: %s", err.Error())
+	}
+
+	if len(sessions) != 1 {
+		t.Error("Expected 1 chat session, but got", len(sessions))
+	}
+
+	session := sessions[0]
+	if session.Name != "Test Chat Session" {
+		t.Errorf("Expected chat session name 'Test Chat Session', but got '%s'", session.Name)
+	}
+
+	// Test UpdateName method
+	err = csr.UpdateName(session.ID, "Updated Chat Session")
+	if err != nil {
+		t.Errorf("Error updating chat session name: %s", err.Error())
+	}
+
+	// Verify updated name
+	sessions, err = csr.QueryByUserID(testUser.Id)
+	if err != nil {
+		t.Errorf("Error querying chat sessions: %s", err.Error())
+	}
+
+	if len(sessions) != 1 {
+		t.Error("Expected 1 chat session, but got", len(sessions))
+	}
+
+	session = sessions[0]
+	if session.Name != "Updated Chat Session" {
+		t.Errorf("Expected chat session name 'Updated Chat Session', but got '%s'", session.Name)
+	}
+
+	// Test DeleteAll method
+	err = csr.DeleteAll(testUser.Id)
+	if err != nil {
+		t.Errorf("Error deleting chat sessions: %s", err.Error())
+	}
+
+	// Verify that all chat sessions have been deleted
+	sessions, err = csr.QueryByUserID(testUser.Id)
+	if err != nil {
+		t.Errorf("Error querying chat sessions: %s", err.Error())
+	}
+
+	if len(sessions) != 0 {
+		t.Error("Expected 0 chat sessions, but got", len(sessions))
+	}
+}
+
+func TestChatSessionParticipant(t *testing.T) {
+	// Create a mock user
+	usersR := &UserRepository{}
+	mockUser := createMockUser()
+	testUser, close := userMock(t, usersR, mockUser)
+	defer close()
+
+	// Create a mock chat session
+	csr := &ChatSessionRepository{}
+	err := csr.Create(testUser.Id, "public_key", "Test Chat Session")
+	if err != nil {
+		t.Fatalf("Error creating chat session: %s", err.Error())
+	}
+	sessions, err := csr.QueryByUserID(testUser.Id)
+	if err != nil {
+		t.Fatalf("Error querying chat sessions: %s", err.Error())
+	}
+	if len(sessions) != 1 {
+		t.Fatal("Expected 1 chat session, but got", len(sessions))
+	}
+	session := sessions[0]
+	defer csr.DeleteAll(testUser.Id)
+
+	// Create the ChatSessionParticipantRepository instance
+	cspr := &ChatSessionParticipantRepository{}
+
+	// Test Create method
+	err = cspr.Create(session.ID, testUser.Id)
+	if err != nil {
+		t.Errorf("Error creating chat session participant: %s", err.Error())
+	}
+
+	// Test QueryBySessionID method
+	participants, err := cspr.QueryBySessionID(session.ID)
+	if err != nil {
+		t.Errorf("Error querying chat session participants: %s", err.Error())
+	}
+
+	if len(participants) != 1 {
+		t.Error("Expected 1 chat session participant, but got", len(participants))
+	}
+
+	participant := participants[0]
+	if participant.UserID != testUser.Id {
+		t.Errorf("Expected chat session participant user ID '%d', but got '%d'", testUser.Id, participant.UserID)
+	}
+
+	// Test DeleteAll method
+	err = cspr.DeleteAll(session.ID)
+	if err != nil {
+		t.Errorf("Error deleting chat session participants: %s", err.Error())
+	}
+
+	// Verify that all chat session participants have been deleted
+	participants, err = cspr.QueryBySessionID(session.ID)
+	if err != nil {
+		t.Errorf("Error querying chat session participants: %s", err.Error())
+	}
+
+	if len(participants) != 0 {
+		t.Error("Expected 0 chat session participants, but got", len(participants))
+	}
+}
+
+func TestChat(t *testing.T) {
+	// Create a mock user
+	usersR := &UserRepository{}
+	mockUser := createMockUser()
+	testUser, close := userMock(t, usersR, mockUser)
+	defer close()
+
+	// Create a mock chat session
+	csr := &ChatSessionRepository{}
+	err := csr.Create(testUser.Id, "public_key", "Test Chat Session")
+	if err != nil {
+		t.Fatalf("Error creating chat session: %s", err.Error())
+	}
+	sessions, err := csr.QueryByUserID(testUser.Id)
+	if err != nil {
+		t.Fatalf("Error querying chat sessions: %s", err.Error())
+	}
+	if len(sessions) != 1 {
+		t.Fatal("Expected 1 chat session, but got", len(sessions))
+	}
+	session := sessions[0]
+
+	// Create the ChatRepository instance
+	cr := &ChatRepository{}
+
+	// Test Create method
+	err = cr.Create(session.ID, testUser.Id, "Test Chat")
+	if err != nil {
+		t.Errorf("Error creating chat: %s", err.Error())
+	}
+
+	// Test QueryBySessionID method
+	chats, err := cr.QueryBySessionID(session.ID)
+	if err != nil {
+		t.Errorf("Error querying chats: %s", err.Error())
+	}
+
+	if len(chats) != 1 {
+		t.Error("Expected 1 chat, but got", len(chats))
+	}
+
+	chat := chats[0]
+	if chat.Content != "Test Chat" {
+		t.Errorf("Expected chat content 'Test Chat', but got '%s'", chat.Content)
+	}
+
+	// Test DeleteBySessionAndUser method
+	err = cr.DeleteBySessionAndUser(session.ID, testUser.Id)
+	if err != nil {
+		t.Errorf("Error deleting chat: %s", err.Error())
+	}
+
+	// Verify that the chat has been deleted
+	chats, err = cr.QueryBySessionID(session.ID)
+	if err != nil {
+		t.Errorf("Error querying chats: %s", err.Error())
+	}
+
+	if len(chats) != 0 {
+		t.Error("Expected 0 chats, but got", len(chats))
+	}
+}
