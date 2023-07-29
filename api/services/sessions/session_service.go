b364c7303229fd537ffef2ef1e1bc68d540dd87c
yyyoichi <origamiichiro@gmail.com>
2023-07-29 08:34:55 +0000

    feat: create session service and mock

---
+++ api/services/sessions/session_service.go
@@ -0,0 +1,164 @@
+package sessions
+
+import (
+	"database/sql"
+	"errors"
+	"himakiwa/services/database"
+	"time"
+)
+
+var (
+	ErrCannotAccessSession = errors.New("cannot access session")
+)
+
+type SessionServices struct {
+	repositories *database.SessionRepositories
+	loginUserID  int
+}
+
+func NewSessionServices(loginUserID int) *SessionServices {
+	return &SessionServices{database.NewSessionRepositories(), loginUserID}
+}
+
+// get active and archived sessions
+func (ss *SessionServices) GetActiveOrArchivedSessions() ([]*database.TQuerySessions, error) {
+	options := database.TQuerySessionsOptions{
+		InPartyStatus:   []database.TParticipantStatus{database.TJoinedParty, database.TInvitedParty},
+		InSessionStatus: []database.TSessionStatus{database.TActiveSession, database.TArchivedSession},
+	}
+	var sessions []*database.TQuerySessions
+	err := database.WithTransaction(func(tx *sql.Tx) error {
+		var err error
+		sessions, err = ss.repositories.SessionRepository.QueryByUserID(tx, ss.loginUserID, options)
+		return err
+	})
+	if err != nil {
+		return nil, err
+	}
+	return sessions, nil
+}
+
+// create session and loginUser invite userID
+func (ss *SessionServices) CreateSession(publicKey, name string, userID int) error {
+	return database.WithTransaction(func(tx *sql.Tx) error {
+		sessionID, err := ss.repositories.SessionRepository.Create(tx, ss.loginUserID, publicKey, name)
+		if err != nil {
+			return err
+		}
+		_, err = ss.repositories.SessionParticipantRepository.Create(tx, sessionID, ss.loginUserID, ss.loginUserID, database.TJoinedParty)
+		if err != nil {
+			return err
+		}
+		_, err = ss.repositories.SessionParticipantRepository.Create(tx, sessionID, userID, ss.loginUserID, database.TInvitedParty)
+		return err
+	})
+}
+
+// get session and participants
+func (ss *SessionServices) GetSessionAt(sessionID, userID int) (*database.TQuerySessions, []*database.TSessionParticipant, error) {
+	var session *database.TQuerySessions
+	var participants []*database.TSessionParticipant
+	err := database.WithTransaction(func(tx *sql.Tx) error {
+		// get session
+		var err error
+		session, err = ss.repositories.SessionRepository.QueryBySessionUserID(tx, sessionID, userID)
+		if err != nil {
+			return err
+		}
+		// get participants
+		participants, err = ss.repositories.SessionParticipantRepository.QueryBySessionID(tx, sessionID)
+		return err
+	})
+
+	if err != nil {
+		return nil, nil, err
+	}
+	return session, participants, nil
+}
+
+func (ss *SessionServices) UpdateSessionNameAt(sessionID int, name string) error {
+	return database.WithTransaction(func(tx *sql.Tx) error {
+		ok, err := ss.repositories.SessionRepository.HasStatusAt(tx, sessionID, ss.loginUserID, []database.TParticipantStatus{database.TJoinedParty})
+		if err != nil {
+			return err
+		}
+		if !ok {
+			return ErrCannotAccessSession
+		}
+		err = ss.repositories.SessionRepository.UpdateName(tx, sessionID, name)
+		return err
+	})
+}
+
+// chats
+
+// get last chat in acvite sessions in whith the user joins
+func (ss *SessionServices) GetLastChatInActiveSessions() ([]*database.TQueryLastChat, error) {
+	var lastChats []*database.TQueryLastChat
+	err := database.WithTransaction(func(tx *sql.Tx) error {
+		var err error
+		lastChats, err = ss.repositories.SessionChatRepository.QueryLastChatInActiveSessions(tx, ss.loginUserID)
+		return err
+	})
+
+	if err != nil {
+		return nil, err
+	}
+	return lastChats, nil
+}
+
+// get chats in session of [sessionID] in 48 hours
+func (ss *SessionServices) GetChatsAtIn48Hours(sessionID int) ([]*database.TQuerySessionChat, error) {
+	var chats []*database.TQuerySessionChat
+	inRange := database.TQuerySessionChatInRange{
+		StartDate: time.Now().Add(-48 * time.Hour),
+		EndDate:   time.Now(),
+	}
+	err := database.WithTransaction(func(tx *sql.Tx) error {
+		ok, err := ss.repositories.SessionRepository.HasStatusAt(tx, sessionID, ss.loginUserID, []database.TParticipantStatus{database.TJoinedParty})
+		if err != nil {
+			return err
+		}
+		if !ok {
+			return ErrCannotAccessSession
+		}
+		chats, err = ss.repositories.SessionChatRepository.QueryBySessionIDInRange(tx, sessionID, inRange)
+		return err
+	})
+
+	if err != nil {
+		return nil, err
+	}
+	return chats, nil
+}
+
+// send chat content
+func (ss *SessionServices) SendChatAt(sessionID int, content string) error {
+	return database.WithTransaction(func(tx *sql.Tx) error {
+		ok, err := ss.repositories.SessionRepository.HasStatusAt(tx, sessionID, ss.loginUserID, []database.TParticipantStatus{database.TJoinedParty})
+		if err != nil {
+			return err
+		}
+		if !ok {
+			return ErrCannotAccessSession
+		}
+		_, err = ss.repositories.SessionChatRepository.Create(tx, sessionID, ss.loginUserID, content)
+		return err
+	})
+}
+
+// update participant status
+func (ss *SessionServices) UpdateParticipantStatusAt(sessionID, userID int, status database.TParticipantStatus) error {
+	return database.WithTransaction(func(tx *sql.Tx) error {
+		ok, err := ss.repositories.SessionRepository.HasStatusAt(tx, sessionID, ss.loginUserID, []database.TParticipantStatus{database.TJoinedParty})
+		if err != nil {
+			return err
+		}
+		if !ok {
+			return ErrCannotAccessSession
+		}
+
+		err = ss.repositories.SessionParticipantRepository.UpdateStatusBySessionUserID(tx, sessionID, userID, status)
+		return err
+	})
+}
---
+++ api/services/sessions/session_service_mock.go
@@ -0,0 +1,51 @@
+package sessions
+
+import (
+	"database/sql"
+	"himakiwa/services/database"
+)
+
+////////////////////////////////////
+//////./ mock services /////////////
+////////////////////////////////////
+
+func NewSessionServicesMock() *SessionServices {
+	userID1 := 1
+	userID2 := 2
+	tx := &sql.Tx{}
+
+	var sessionID int
+	// loginUser is userID1 //
+	loginUserID := userID1
+	ss := &SessionServices{database.NewSessionRepositoriesMock(), loginUserID}
+
+	// session1 invite userID2
+	ss.CreateSession("", "Session1", userID2)
+
+	// session2 reject userID2
+	sessionID = 2
+	ss.CreateSession("", "Session2", userID2)
+	ss.UpdateParticipantStatusAt(sessionID, userID2, database.TRejectedParty)
+
+	// session3 joined userID2 and chats
+	sessionID = 3
+	ss.CreateSession("", "Session3", userID2)
+	ss.UpdateParticipantStatusAt(sessionID, userID2, database.TJoinedParty)
+	ss.SendChatAt(3, "Hello, I am 1 in the session 3")
+	ss.repositories.SessionChatRepository.Create(tx, sessionID, userID2, "Hello, I am 2 in the session 3")
+
+	// session4 is same session3, session4 is arcived
+	sessionID = 4
+	ss.CreateSession("", "Session4", userID2)
+	ss.UpdateParticipantStatusAt(sessionID, userID2, database.TJoinedParty)
+	ss.repositories.SessionChatRepository.Create(tx, sessionID, userID2, "Hello, I am 2 in the session 4")
+	ss.SendChatAt(sessionID, "Hello, I am 1 in the session 4")
+	ss.repositories.SessionRepository.UpdateStatus(tx, sessionID, database.TArchivedSession)
+
+	// session5 is breakup
+	sessionID = 5
+	ss.CreateSession("", "Session5", userID2)
+	ss.repositories.SessionRepository.UpdateStatus(tx, sessionID, database.TBreakupSession)
+
+	return ss
+}
